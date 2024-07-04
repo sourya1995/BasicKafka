04892e0e609f92b748ad78d3db0d2197f29be20e
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2024-07-04 13:40:31 +0530

    basic representation of a Topic

---
+++ Topic.java
@@ -0,0 +1,41 @@
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.logging.Logger;
+
+public class Topic {
+    private static final Logger LOGGER = Logger.getLogger(Topic.class.getName());
+    private String name;
+    private int numPartitions;
+    private BlockingQueue<Message>[] partitions;
+
+    @SuppressWarnings("unchecked")
+    public Topic(String name, int numPartitions) {
+        this.name = name;
+        this.numPartitions = numPartitions;
+        this.partitions = new LinkedBlockingQueue[numPartitions];
+        for (int i = 0; i < numPartitions; i++){
+            partitions[i] = new LinkedBlockingQueue<>();
+        }
+    }
+
+    public void produce(String messageContent){
+        int partition = Math.abs(messageContent.hashCode()) % numPartitions;
+        Message message = new Message(messageContent);
+        partitions[partition].offer(message);
+        LOGGER.info(() -> "Produced to topic" + name + "partition" + partition + ":" + message);
+
+    }
+
+    public Message consume(String consumerId){
+        int partition = Math.abs(consumerId.hashCode()) % numPartitions;
+        Message message = partitions[partition].poll();
+        if (message != null){
+            LOGGER.info(() -> "Consumed from topic" + name + "partition" + partition + ":" + message);
+        }
+
+        return message;
+    }
+
+    
+}
